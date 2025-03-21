99314383eff7db3f88daa744dad758f71bfc0706
Srinivasan Kannan Ramarao <srinivasan.ramarao@oracle.com>
2025-03-21 14:03:39 -0700

    Adding subarray contiguous elements whose sum equals to k

---
+++ problems/subarraySum.py
@@ -0,0 +1,42 @@
+#560. Subarray Sum Equals K
+'''
+Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,1], k = 2
+Output: 2
+Example 2:
+
+Input: nums = [1,2,3], k = 3
+Output: 2
+ 
+
+Constraints:
+
+1 <= nums.length <= 2 * 104
+-1000 <= nums[i] <= 1000
+-107 <= k <= 107
+'''
+
+def subarraySum(nums, k):
+    seqCount = 0 
+    sum = 0
+    d = dict()
+    d[0] = 1
+    for i in range(len(nums)):
+        sum += nums[i]
+        seqCount += d.get(sum-k,0)
+        d[sum] = d.get(sum,0) + 1
+
+    return seqCount 
+
+#nums = [1,1,1]
+#k = 2
+nums = [1,2,3]
+k = 3
+print(subarraySum(nums, k))
