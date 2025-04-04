762754bf340f0fa0ff5e431e9354e149fa1db7de
Srinivasan Kannan Ramarao <srinivasan.ramarao@oracle.com>
2025-04-04 13:42:07 -0700

    Adding comments to the validParen.py

--- problems/LinkedIn-Prep/validParen.py
+++ problems/LinkedIn-Prep/validParen.py
@@ -1,5 +1,32 @@
 '''
 20. Valid Parentheses
+
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+
+Example 1:
+    Input: s = "()"
+    Output: true
+
+Example 2:
+    Input: s = "()[]{}"
+    Output: true
+
+Example 3:
+    Input: s = "(]"
+    Output: false
+
+Example 4:
+    Input: s = "([])"
+    Output: true
+
+Time complexity : O(n)
+Space complexity : O(n)
 '''
 
 def validParen(s):
